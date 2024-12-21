23f74bd61177b47c3c21338678b7fb9bb15afe19
kwakseobang <iii6602@naver.com>
2024-12-22 00:56:11 +0900

    UserDefault Manager 구현

--- VoteApp/General/UserDefaultsManager.swift
+++ VoteApp/General/UserDefaultsManager.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+class UserDefaultsManager {
+    enum UserDefaultsKeys: String, CaseIterable {
+        case accessToken
+    }
+    
+    static func setData<T>(value: T, key: UserDefaultsKeys) {
+        let defaults = UserDefaults.standard
+        defaults.set(value, forKey: key.rawValue)
+    }
+    
+    static func getData<T>(type: T.Type, forKey: UserDefaultsKeys) -> T? {
+        let defaults = UserDefaults.standard
+        let value = defaults.object(forKey: forKey.rawValue) as? T
+        return value
+    }
+    
+    static func removeData(key: UserDefaultsKeys) {
+        let defaults = UserDefaults.standard
+        defaults.removeObject(forKey: key.rawValue)
+    }
+}
